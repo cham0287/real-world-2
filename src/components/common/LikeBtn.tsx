d81d6fae736ada9d6e30c36cbe83a86282c31fa2
cham0287 <crtmt97@gmail.com>
2023-12-28 19:28:54 +0900

    Add: react-icons 추가

--- src/components/common/LikeBtn.tsx
+++ src/components/common/LikeBtn.tsx
@@ -1,10 +1,14 @@
 import { HTMLAttributes } from 'react';
+import { FaHeart } from 'react-icons/fa';
 
 const LikeBtn = ({ children, ...props }: HTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
       className='rounded-[3px] border border-primary text-primary text-xs px-2 py-1 flex items-center hover:text-white hover:bg-primary'
-      {...props}>
+      {...props}
+    >
+      <FaHeart />
+      &nbsp;
       {children}
     </button>
   );
