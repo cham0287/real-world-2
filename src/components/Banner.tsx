3bac820191e89be699b1fd4d4da5da61e1876981
dhkim0306 <crtmt97@gmail.com>
2023-12-28 11:20:32 +0900

    Feat: Banner 추가

---
+++ src/components/Banner.tsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+  return (
+    <div className='bg-[#5cb85c] text-white flex flex-col gap-4 py-10 justify-center items-center'>
+      <h1 className='text-5xl font-bold drop-shadow-lg'>conduit</h1>
+      <p className='text-xl font-light'>A place to share your knowledge.</p>
+    </div>
+  );
+};
+
+export default Banner;
