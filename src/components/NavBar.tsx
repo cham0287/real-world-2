f7d2651af2ee446ccd51603c971a8d73eeffac9b
dhkim0306 <crtmt97@gmail.com>
2023-12-28 10:45:45 +0900

    Feat: navbar 기본기능 구현

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,25 @@
 'use client';
-
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NAV_LIST = [
+  { text: 'Home', pathname: '/' },
+  { text: 'Sign in', pathname: '/login' },
+  { text: 'Sign up', pathname: '/register' },
+];
 
 const NavBar = () => {
+  const currentPathname = usePathname();
+
   return (
     <nav className='flex px-10 py-4 justify-between'>
       <div className='text-green-600 font-bold text-xl'>conduit</div>
-      <ul className='flex gap-2'>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/login'>Sign in</Link>
-        </li>
-        <li>
-          <Link href='/register'>Sign up</Link>
-        </li>
+      <ul className='flex gap-4 text-gray-400'>
+        {NAV_LIST.map(({ text, pathname }) => (
+          <li key={pathname} className={`${currentPathname === pathname && 'text-black'}`}>
+            <Link href={pathname}>{text}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
