aae6e6aa3dafceb4ea4d558327e04fcef5cb9ea3
dhkim0306 <crtmt97@gmail.com>
2023-12-29 11:35:33 +0900

    Feat: Article 링크 추가

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,17 +1,36 @@
 import { Article } from '@/types/articles';
 import ArticleAuthor from './ArticleAuthor';
 import LikeBtn from '../common/LikeBtn';
+import Link from 'next/link';
 
-const Article = ({ article: { title, description, author, createdAt, favoritesCount } }: { article: Article }) => {
+const Article = ({
+  article: { title, description, author, createdAt, favoritesCount, tagList, slug },
+}: {
+  article: Article;
+}) => {
   return (
     <div className='flex flex-col gap-4 border-gray-200 border-t py-6'>
       <div className='flex justify-between items-center'>
         <ArticleAuthor author={author} createdAt={createdAt} color='text-primary' />
         <LikeBtn>{favoritesCount}</LikeBtn>
       </div>
-      <section>
+      <Link href={`/article/${slug}`}>
         <h2 className='text-xl font-semibold'>{title}</h2>
         <p className='text-sm text-gray-400 font-light'>{description}</p>
+      </Link>
+      <section className='flex justify-between'>
+        <Link href={`/article/${slug}`} className='text-gray-300 text-xs'>
+          Read more...
+        </Link>
+        <Link href={`/article/${slug}`}>
+          <ul className='flex gap-1'>
+            {tagList.map((tag) => (
+              <li key={tag} className='rounded-full border px-[6px] py-[2px] border-gray-300 text-xs text-gray-300'>
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </Link>
       </section>
     </div>
   );
--- src/components/Article/ArticleAuthor.tsx
+++ src/components/Article/ArticleAuthor.tsx
@@ -2,6 +2,7 @@ import { Author } from '@/types/common';
 import Image from 'next/image';
 import dayjs from 'dayjs';
 import { HTMLAttributes } from 'react';
+import Link from 'next/link';
 
 interface ArticleAuthorProps extends HTMLAttributes<HTMLDivElement> {
   author: Author;
@@ -16,7 +17,9 @@ const ArticleAuthor = ({ author, createdAt, color }: ArticleAuthorProps) => {
     <div className={`flex gap-2 items-center ${color}`}>
       <Image className='rounded-full' src={author.image} alt='author image' width={30} height={30} />
       <div>
-        <p className='text-sm'>{author.username}</p>
+        <Link href={`/author/${author.username}`} className='text-sm hover:underline hover:text-green-700'>
+          {author.username}
+        </Link>
         <p className='text-xs text-gray-300'>{formattedDate}</p>
       </div>
     </div>
